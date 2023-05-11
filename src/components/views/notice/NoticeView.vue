fd372eb2b66f7eb8be91c3326b42e623642b4cb1
oripkgo <oripkgo@gmail.com>
2023-05-11 23:07:18 +0900

    공지사항 view 화면 개발

--- src/components/views/notice/NoticeView.vue
+++ src/components/views/notice/NoticeView.vue
@@ -1,16 +1,14 @@
 
-<!--<c:set var="vo" value="${result.view}"/>-->
-
 <template>
   <div class="section">
     <div class="ani-in sub_layout">
 
       <div class="detail_top ani_y delay1">
         <div class="detail_kind">공지사항</div>
-        <strong>${vo.TITLE}</strong>
+        <strong>{{vo['TITLE']}}</strong>
         <div class="detail_memo">
-          <em>by ${vo.NICKNAME}</em>
-          <img src="/resources/img/line.png">
+          <em>by {{vo['NICKNAME']}}</em>
+          <img src="@/resources/img/line.png">
           <span id="last_time"></span>
         </div>
       </div>
@@ -23,12 +21,10 @@
     <div class="section uline2">
       <div class="ani-in sub_layout rline">
         <div class="conts_wrap ani_y delay2">
-
-          ${vo.CONTENTS}
+          <div id="noticeContents"></div>
 
           <div class="conts_sns">
-            <a href="javascript:;" class="zimm like" data-likecnt="${vo.LIKE_CNT}">공감 ${vo.LIKE_CNT}</a>
-
+            <a href="javascript:;" class="zimm like" :data-likecnt="vo['LIKE_CNT']">공감 {{vo['LIKE_CNT']}}</a>
           </div>
 
           <div class="conts_tag" style="display: none;">
@@ -36,7 +32,6 @@
           </div>
 
           <div class="conts_review"></div>
-
         </div>
       </div>
     </div>
@@ -49,11 +44,28 @@ import comm from "@/resources/task/js/common/comm.js";
 
   export default {
       data(){
-        return {
+        const data = this.getNoticeInfo();
+
+        const result = {
           type : 'NOTICE',
-          id : '${vo.ID}',
-          regDate : '${vo.REG_DATE}',
+          id : null,
+          regDate : null,
+          vo : {},
+        };
+
+        if( data.view ){
+          result['vo'] = data.view;
         }
+
+        if( result['vo'] && result['vo']['ID'] ){
+          result['id'] = result['vo']['ID'];
+        }
+
+        if( result['vo'] && result['vo']['REG_DATE'] ){
+          result['regDate'] = result['vo']['REG_DATE'];
+        }
+
+        return result
       },
 
       mounted() {
@@ -61,11 +73,37 @@ import comm from "@/resources/task/js/common/comm.js";
         // 지난 시간 세팅
         $("#last_time").html( comm.last_time_cal($this.regDate) );
 
-        comm.initBoardView($this.type, $this.id, null,{"likeTarget":".like", "tagsTarget":".conts_tag", "commentTarget":".conts_review"});
+        $("#noticeContents").replaceWith($this.vo['CONTENTS'])
+
+        comm.initBoardView($this.type, $this.id, function(){},{"likeTarget":".like", "tagsTarget":".conts_tag", "commentTarget":".conts_review"});
 
         $(".sns_btn").click(function () {
           $(".sns_view").slideToggle("fast");
         });
-      }
+      },
+
+      methods : {
+        getNoticeInfo: function () {
+          const $this = this;
+          let data = {};
+          let path = $this.$route['fullPath'];
+
+          comm.request({
+            url: path,
+            method: "GET",
+            async: false
+          }, function (resp) {
+            // 삭제 성공
+            if (resp.code == '0000') {
+              data = {
+                modifyAuthorityYn: resp['modify_authority_yn'],
+                view: resp['view'],
+              };
+            }
+          })
+
+          return data;
+        },
+      },
   }
 </script>
